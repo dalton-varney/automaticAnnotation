9f0192cccbeace04c9da2a9b0e7dd701a09cd38b
dalton-varney <dalton.varney@alwaysai.co>
2021-11-22 16:02:13 -0800

    Added Application Files

---
+++ README.md
@@ -0,0 +1,43 @@
+# Object Detector
+Use [Object Detection](https://alwaysai.co/docs/application_development/core_computer_vision_services.html#object-detection) to detect objects in a real-time camera stream. The types of objects detected can be changed by selecting different models.
+
+## Requirements
+* [alwaysAI account](https://alwaysai.co/auth?register=true)
+* [alwaysAI Development Tools](https://alwaysai.co/docs/get_started/development_computer_setup.html)
+
+## Usage
+Once the alwaysAI tools are installed on your development machine (or edge device if developing directly on it) you can install and run the app with the following CLI commands:
+
+To perform initial configuration of the app:
+```
+aai app configure
+```
+
+To prepare the runtime environment and install app dependencies:
+```
+aai app install
+```
+
+To start the app:
+```
+aai app start
+```
+
+###Configuration Parameters
+To run this app pass the appropriate configuration parameters to the `auto_annotate` method:
+```
+auto_annotate(dataset_name: str, video_path: str, model_id: str, labels: List[str], confidence_level: float, overlap_threshold: float, markup_image: bool)
+```
+
+Example:
+```
+auto_annotate('output_center_cam_demos', 'CenterCam.avi', 'ripteyedprod/posup-sides-m75-8-med', ["TwixRegular", "KitkatRegular", "MtnDewTwenty", "DeepRiverJalapeno", "KitkatLarge", "DietPepsiTwentyOz", "AquafinaTwentyOz", "TwixLarge", "TrolliSourWorms", "PepsiTwentyOz", "BakedLaysXLV", "DeepRiverOriginal", "Starburst", "DoritosXLV", "SabraHummus", "SabraRedPepper"], 0.2, 0.1, True)
+```
+
+###More Information
+To change the computer vision model, the engine and accelerator, and add additional dependencies read [this guide](https://alwaysai.co/docs/application_development/configuration_and_packaging.html).
+
+## Support
+* [Documentation](https://alwaysai.co/docs/)
+* [Community Discord](https://discord.gg/z3t9pea)
+* Email: support@alwaysai.co
---
+++ app.py
@@ -0,0 +1,141 @@
+"""
+Auto Annotation Application
+
+Lets you chose an alwaysai model and a video to annotate. You can choose labels to annotate and toy around with
+confidence level.
+
+Look at the performance of the annotations on the streamer.
+
+To upload to CVAT upload the files in jpeg images to a task. Then click Add annotations and upload the annotations zip
+"""
+import os
+import shutil
+from typing import List
+import zipfile
+
+import cv2
+import edgeiq
+from lxml import etree
+from lxml.builder import E
+import numpy
+
+
+class AutoAnnotator(object):
+    def __init__(self, video_stream: edgeiq.FileVideoStream, detector: edgeiq.ObjectDetection, confidence_level: float,
+                 overlap_threshold: float, labels: List[str], markup_image: bool) -> None:
+
+        self.image_index = 0
+        self.video_stream = video_stream
+        self.detector = detector
+        self.confidence_level = confidence_level
+        self.overlap_threshold = overlap_threshold
+        for label in labels:
+            if label not in self.detector.labels:
+                raise RuntimeError(f'Label {label} is not a label {self.detector.model_id} provides')
+        self.labels = labels
+        self.images_path = None
+        self.annotations_path = None
+        self.image_sets_path = None
+        self.markup_image = markup_image
+
+    def annotate(self) -> (etree._ElementTree, numpy.ndarray, str):
+        while self.video_stream.more():
+            frame = self.video_stream.read()
+            image_name = f'{self.image_index:0>7d}.png'  # Doesn't work on videos which product more than 10M images
+            results = self.detector.detect_objects(frame, confidence_level=self.confidence_level)
+            predictions = edgeiq.filter_predictions_by_label(results.predictions, self.labels)
+            annotation_xml = self._annotate_frame(frame, image_name, predictions)
+            if self.markup_image:
+                frame = edgeiq.markup_image(frame, predictions, show_confidences=False, colors=self.detector.colors)
+            yield annotation_xml, frame, image_name
+
+    @staticmethod
+    def _annotate_frame(frame: numpy.ndarray, image_name: str,
+                        predictions: List[edgeiq.ObjectDetectionPrediction]) -> etree._ElementTree:
+        annotation_element = (
+            E.annotation(
+                E.filename(image_name),
+                E.size(
+                    E.width(str(frame.shape[1])),
+                    E.height(str(frame.shape[0])),
+                    E.depth(str(3)),
+                )
+            )
+        )
+        for prediction in predictions:
+            object_xml = (
+                E.object(
+                    E.name(prediction.label),
+                    E.bndbox(
+                        E.xmin(str(prediction.box.start_x)),
+                        E.ymin(str(prediction.box.start_y)),
+                        E.xmax(str(prediction.box.end_x)),
+                        E.ymax(str(prediction.box.end_y)),
+                    )
+                )
+            )
+            annotation_element.append(object_xml)
+        return etree.ElementTree(annotation_element)
+
+    def make_directory_structure(self, dataset_name: str) -> None:
+        if os.path.exists(dataset_name):
+            shutil.rmtree(dataset_name)
+        os.mkdir(dataset_name)
+        annotations_path = os.path.join(dataset_name, 'Annotations')
+        os.mkdir(annotations_path)
+        images_path = os.path.join(dataset_name, 'JPEGImages')
+        os.mkdir(images_path)
+        image_sets_path = os.path.join(dataset_name, 'ImageSets', 'Main')
+        os.makedirs(image_sets_path)
+        self.images_path = images_path
+        self.annotations_path = annotations_path
+        self.image_sets_path = image_sets_path
+
+    def write_image(self, annotation_xml: etree._ElementTree, frame: numpy.ndarray, image_name: str):
+        if self.images_path is None:
+            raise FileNotFoundError('Images directory does not exist')
+        if self.annotations_path is None:
+            raise FileNotFoundError('Annotations directory does not exist')
+        cv2.imwrite(os.path.join(self.images_path, image_name), frame, [cv2.IMWRITE_PNG_COMPRESSION, 0])
+        annotation_xml.write(os.path.join(self.annotations_path, f'{self.image_index:0>7d}.xml'), pretty_print=True)
+
+    def write_default_file(self):
+        if self.image_sets_path is None:
+            raise FileNotFoundError('ImageSets directory does not exist')
+        with open(os.path.join(self.image_sets_path, 'default.txt'), 'w') as default_file:
+            default_file.write('\n'.join([f'{i:0>7d}' for i in range(0, self.image_index)]))
+
+    def zip_annotations(self, dataset_name):
+        if self.image_sets_path is None:
+            raise FileNotFoundError('ImageSets directory does not exist')
+        if self.annotations_path is None:
+            raise FileNotFoundError('Annotations directory does not exist')
+        os.chdir(dataset_name)
+        zip_file = zipfile.ZipFile(f'{dataset_name}_annotations.zip', 'w')
+        for dir_ in [os.path.sep.join(self.image_sets_path.split(os.path.sep)[1:]),
+                     os.path.basename(self.annotations_path)]:
+            for root, dirs, files in os.walk(dir_):
+                for file in files:
+                    zip_file.write(os.path.join(root, file))
+        zip_file.close()
+
+
+def auto_annotate(dataset_name: str, video_path: str, model_id: str, labels: List[str], confidence_level: float, overlap_threshold: float, markup_image: bool):
+    obj_detect = edgeiq.ObjectDetection(model_id)
+    obj_detect.load(engine=edgeiq.Engine.DNN)
+    with edgeiq.FileVideoStream(video_path) as video_stream, edgeiq.Streamer(3, inter_msg_time=1) as streamer:
+        auto_annotator = AutoAnnotator(video_stream, obj_detect, confidence_level, overlap_threshold, labels, markup_image)
+        auto_annotator.make_directory_structure(dataset_name)
+        for annotation_xml, frame, image_name in auto_annotator.annotate():
+            auto_annotator.write_image(annotation_xml, frame, image_name)
+            streamer.send_data(frame, f'Image: {auto_annotator.image_index}')
+            auto_annotator.image_index += 1
+            if streamer.check_exit():
+                break
+        auto_annotator.write_default_file()
+        auto_annotator.zip_annotations(dataset_name)
+    print("Program Ending")
+
+
+if __name__ == '__main__':
+    auto_annotate('output_center_cam_demos', 'CenterCam.avi', 'ripteyedprod/posup-sides-m75-8-med', ["TwixRegular", "KitkatRegular", "MtnDewTwenty", "DeepRiverJalapeno", "KitkatLarge", "DietPepsiTwentyOz", "AquafinaTwentyOz", "TwixLarge", "TrolliSourWorms", "PepsiTwentyOz", "BakedLaysXLV", "DeepRiverOriginal", "Starburst", "DoritosXLV", "SabraHummus", "SabraRedPepper"], 0.2, 0.1, True)
